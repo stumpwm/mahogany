aff7a561d37984c60ada7e84576b1f20dbf81827
sdilts <stuart.dilts@gmail.com>
2018-08-30 13:38:50 -0600

    Change sample-output to class mh-output

--- src/backend/desktop.lisp
+++ src/backend/desktop.lisp
@@ -28,10 +28,16 @@
 	    :type 'list
 	    :initform ())))
 
-(defstruct sample-output
-  output
-  frame-listener
-  destroy-listener)
+(defclass mh-output ()
+  ((wlr-output :initarg :wlr-output
+	      :reader output-wlr-output
+	      :type wlr:output)
+  (frame-listener :initarg :frame-listener
+		  :reader output-frame-listener
+		  :type wl_listener)
+  (destroy-listener :initarg :destroy-listener
+		    :reader destroy-listener
+		    :type wl_listener)))
 
 (defvar *listener-hash* (make-hash-table))
 
@@ -41,14 +47,14 @@
   (declare (ignore output))
   (let* ((output-owner (get-listener-owner listener *listener-hash*))
 	 ;; (renderer (wlr:backend-get-renderer (sample-state-backend *sample-state*)))
-	 (renderer (wlr:backend-get-renderer (foreign-slot-value (sample-output-output output-owner)
+	 (renderer (wlr:backend-get-renderer (foreign-slot-value (output-wlr-output output-owner)
 						       '(:struct wlr:output)
 						       :backend))))
-    (wlr:output-make-current (sample-output-output output-owner) (cffi:null-pointer))
+    (wlr:output-make-current (output-wlr-output output-owner) (cffi:null-pointer))
 
     (with-foreign-array (color #(0.4 0.4 0.4 1.0) '(:array :float 4))
       (wlr:renderer-clear renderer color))
-    (wlr:output-swap-buffers (sample-output-output output-owner) (cffi:null-pointer)
+    (wlr:output-swap-buffers (output-wlr-output output-owner) (cffi:null-pointer)
 			     (cffi:null-pointer))
     (wlr:renderer-end renderer)))
 
@@ -61,16 +67,16 @@
   (multiple-value-bind (desktop-owner lookup-output)
       (values-list (get-listener-owner listener *listener-hash*))
     (assert (eql (pointer-address output)
-		 (pointer-address (sample-output-output lookup-output))))
+		 (pointer-address (output-wlr-output lookup-output))))
     (delete lookup-output (desktop-outputs desktop-owner))
     (unregister-listener listener *listener-hash*)
-    (unregister-listener (sample-output-frame-listener lookup-output) *listener-hash*)
+    (unregister-listener (output-frame-listener lookup-output) *listener-hash*)
     (wl-list-remove (cffi:foreign-slot-pointer listener
     					       '(:struct wl_listener) 'link))
-    (wl-list-remove (cffi:foreign-slot-pointer (sample-output-frame-listener lookup-output)
+    (wl-list-remove (cffi:foreign-slot-pointer (output-frame-listener lookup-output)
     					       '(:struct wl_listener) 'link))
     (cffi:foreign-free listener)
-    (cffi:foreign-free (sample-output-frame-listener lookup-output))))
+    (cffi:foreign-free (output-frame-listener lookup-output))))
 
 (cffi:defcallback handle-new-output :void
     ((listener :pointer)
@@ -94,9 +100,10 @@
     								  '(:struct wlr:output)
     								  :event-destroy)
     				       destroy-listener)
-    (let ((new-output (make-sample-output :output output
-    					  :frame-listener frame-listener
-    					  :destroy-listener destroy-listener)))
+    (let ((new-output (make-instance 'mh-output
+				     :wlr-output output
+    				     :frame-listener frame-listener
+    				     :destroy-listener destroy-listener)))
       ;; insert both the desktop and the output so we don't have to look it up later:
       (register-listener destroy-listener (list desktop-owner new-output) *listener-hash*)
       (register-listener frame-listener new-output *listener-hash*)
