9eb3912c8e9c0f23783ef3c999d963f0311ef625
sdilts <stuart.dilts@gmail.com>
2018-12-07 09:47:54 -0700

    Re-order exports and export additional symbols

--- package.lisp
+++ package.lisp
@@ -1,31 +1,37 @@
 (defpackage #:mahogany/log
-  (:use :cl :cl-ansi-text))
+  (:use :cl #:cl-ansi-text))
 
 (defpackage #:mahogany/util
   (:use #:cl))
 
 (defpackage #:mahogany/tree
   (:use :cl #:mahogany/log #:alexandria #:mahogany/util #:iterate)
-  (:export #:split-frame-h
-	   #:split-frame-v
-	   #:swap-positions
-	   #:*split-frame-hook*
+  (:export #:*split-frame-hook*
 	   #:*new-frame-hook*
 	   #:*remove-split-hook*
 	   #:*new-split-type*
 	   #:frame
-	   #:tree-frame
-	   #:binary-tree-frame
-	   #:poly-tree-frame
-	   #:root-frame-p
 	   #:frame-x
 	   #:frame-y
 	   #:frame-width
 	   #:frame-height
 	   #:frame-parent
 	   #:tree-container
+	   #:root-tree
+	   #:tree-frame
 	   #:tree-children
-	   #:root-tree))
+	   #:tree-split-direction
+	   #:binary-tree-frame
+	   #:poly-tree-frame
+	   #:split-frame-v
+	   #:split-frame-h
+	   #:remove-frame
+	   #:replace-frame
+	   #:swap-positions
+	   #:find-empty-frame
+	   #:get-empty-frames
+	   #:set-dimensions
+	   #:root-frame-p))
 
 (defpackage #:mahogany/backend
   (:use :cl :cffi #:mahogany/log #:alexandria #:wlr/macros #:wlr/common-c-types)
