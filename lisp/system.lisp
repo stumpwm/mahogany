5945ee955c6962885e6779c5cf51aae23fc8d466
Stuart Dilts <stuart.dilts@gmail.com>
2023-10-26 20:30:23 -0600

    Add missing file

---
+++ lisp/system.lisp
@@ -0,0 +1,26 @@
+(defpackage #:mahogany/system
+  (:documentation "Package for functions interacting with the system that
+Mahogany is running under")
+  (:use :cl)
+  (:local-nicknames (#:alex #:alexandria))
+  (:nicknames #:sys)
+  (:export #:find-program
+	   #:open-terminal))
+
+(in-package #:mahogany/system)
+
+(defun find-program (name)
+  (declare (type string name))
+  (handler-case
+      (string-trim '(#\Newline #\Space)
+		   (with-output-to-string (stream)
+		     (uiop:run-program (list "which" name) :output stream)))
+    (UIOP/RUN-PROGRAM:SUBPROCESS-ERROR nil)))
+
+(defun open-terminal ()
+  (let ((programs #("konsole" "gnome-terminal")))
+    (loop for i across programs
+	  do (alex:when-let ((program (find-program i)))
+	       (uiop:launch-program program)
+	       (return t)))
+    (values nil)))
