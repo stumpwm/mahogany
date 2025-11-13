ecee92119273a30d3db1cc79db3d3e9b5656b54b
Stuart Dilts <stuart.dilts@gmail.com>
2025-11-12 19:30:18 -0700

    Place lisp build artifacts in build directory
    Put the fasl files in the build directory so that `make clean`
    actually removes them.

--- build-mahogany.lisp
+++ build-mahogany.lisp
@@ -1,19 +1,9 @@
 (require 'asdf)
 
-;; See https://asdf.common-lisp.dev/asdf.html#Configuration-DSL-1
-;; for what this is doing.
-;; Basically, we want our local copies of dependencies in the dependencies folder
-;; to be used instead of anything that asdf can find in our environment
-(asdf:initialize-source-registry
- `(:source-registry
-   (:directory ,(uiop/os:getcwd))
-   (:tree ,(merge-pathnames (uiop/os:getcwd) #P"dependencies"))
-   ;; Use whatever the user has configured in their environment to find the rest.
-   :inherit-configuration))
-
-;; (asdf:find-system "mahogany/executable")
-;; (asdf:clear-configuration)
+(load "init-build-env.lisp")
 
 (declaim (optimize (debug 3)))
+
+(cl:pushnew :hrt-debug *features*)
 
 (asdf:make "mahogany/executable")
