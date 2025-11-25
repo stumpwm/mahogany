stumpwm/mahogany

commit 64e344579488254b37a4fb16e3b061bf2821c0c6

lisp: Create a tree-parent class so more code can be shared
This class represents any node in the frame tree that has children.

This is a bit of an awkward setup, as there's a class-precedence issue
between the tree-frame type and the other parent types; we have to be
careful not to override the frame methods that the tree-frame class
needs to use.

lisp/tree/frame.lisp
@@ -514,7 +514,7 @@ REMOVE-FUNC is called with one argument: the view that was removed."
 	(unless (frame-view frame)
 	  (return-from find-empty-frame frame))))
 
-(defmethod find-empty-frame ((root tree-container))
+(defmethod find-empty-frame ((root tree-parent))
   (dolist (tree (tree-children root))
     (alexandria:when-let (empty (find-empty-frame tree))
       (return-from find-empty-frame empty)))
@@ -529,23 +529,36 @@ REMOVE-FUNC is called with one argument: the view that was removed."
   (remove-if (lambda (x) (not (frame-view x)))
 	     (snakes:generator->list (leafs-in root))))
 
-(defun in-frame-p (frame x y)
-  (declare (type frame frame))
-  (with-accessors ((frame-x frame-x)
-		   (frame-y frame-y)
-		   (frame-width frame-width)
-		   (frame-height frame-height))
-      frame
+;; This is really awkward, as we don't want to
+;; declare an in-frame-p method for the tree-parent class,
+;; as we would overshadow the implementation for the `tree-frame`
+;; type, which needs to use the regular `frame` implementation:
+(defmethod in-frame-p ((parent output-node) x y)
+  ;; output nodes have exactly 1 child:
+  (let ((f (car (tree-children parent))))
+    (in-frame-p f x y)))
+
+(defmethod frame-at ((parent output-node) x y)
+  ;; output nodes have exactly 1 child:
+  (let ((f (car (tree-children parent))))
+    (frame-at f x y)))
+
+(defmethod in-frame-p ((frame frame) x y)
+  (declare (type real x y))
+  (let ((frame-x (frame-x frame))
+	(frame-y (frame-y frame))
+	(frame-width (frame-width frame))
+	(frame-height (frame-height frame)))
     (and (<= frame-x x)
 	 (<  x (+ frame-x frame-width))
 	 (<= frame-y y)
 	 (<  y (+ frame-y frame-height)))))
 
-(defmethod frame-at ((root tree-frame) x y)
+(defmethod frame-at ((root tree-parent) x y)
   (declare (type real x y))
   (dolist (cur-frame (tree-children root))
-      (when (in-frame-p cur-frame x y)
-	(return-from frame-at (frame-at cur-frame x y)))))
+    (when (in-frame-p cur-frame x y)
+      (return-from frame-at (frame-at cur-frame x y)))))
 
 (defmethod frame-at ((frame frame) x y)
   (declare (type real x y))

lisp/tree/tree-interface.lisp
@@ -54,20 +54,19 @@ of an already existing frame with the `set-split-frame-type` function")
 (defgeneric (setf %frame-next) (next frame)
   (:documentation "Set tne next frame for this frame"))
 
-(defclass tree-container ()
-  ((trees :initarg :root
-	  :initform nil
-	  :accessor tree-children
-	  :type list
-	  :documentation "Holds the trees of this conatiner"))
+(defclass tree-parent ()
+  ((children :initarg :children
+	 :initform nil
+	 :accessor tree-children
+	 :type list
+	 :documentation "Holds the trees of this conatiner")))
+
+(defclass tree-container (tree-parent)
+  ()
   (:documentation "A class that contains a frame-tree"))
 
-(defclass output-node ()
-  ((children :initarg children
-	    :initform nil
-	    :accessor tree-children
-	    :type list)
-   (parent :initarg :parent
+(defclass output-node (tree-parent)
+  ((parent :initarg :parent
 	   :initform nil
 	   :type (or null tree-container)
 	   :accessor frame-parent)))
@@ -75,12 +74,8 @@ of an already existing frame with the `set-split-frame-type` function")
 (deftype split-frame-type ()
   '(member :vertical :horizontal))
 
-(defclass tree-frame (frame)
-  ((children :initarg :children
-	     :initform nil
-	    :accessor tree-children
-	    :type list)
-   (split-direction :initarg :split-direction
+(defclass tree-frame (tree-parent frame)
+  ((split-direction :initarg :split-direction
 		    :reader tree-split-direction
 		    :type split-frame-type))
   (:documentation "An inner node of a frame-tree"))

commit de72d8560b56e55b4735625e671b52d565169722

Fix tree:remove-frame-from-parent for tree-parent objects
The tree-frame class doesn't capture all of the types that need to
have this :after method; the tree-parent one does.

Probably broken in 64e3445.

lisp/tree/frame.lisp
@@ -422,7 +422,7 @@ REMOVE-FUNC is called with one argument: the view that was removed."
 (defmethod remove-frame-from-parent :before (parent (frame frame) cleanup-func)
   (assert (equal (frame-parent frame) parent)))
 
-(defmethod remove-frame-from-parent :after ((parent tree-frame) frame cleanup-func)
+(defmethod remove-frame-from-parent :after ((parent tree-parent) frame cleanup-func)
   (declare (ignore parent))
   (release-frames frame cleanup-func))
 