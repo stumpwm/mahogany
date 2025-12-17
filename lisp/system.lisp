743ac2ab032247da585498a9500057fc22bd1b3b
jcguu95 <jcguu95@gmail.com>
2025-12-17 10:56:17 -0700

    Abstract the opener.
    So now it supports all kinds of applications.

--- lisp/system.lisp
+++ lisp/system.lisp
@@ -17,22 +17,19 @@ Mahogany is running under")
 		     (uiop:run-program (list "which" name) :output stream)))
     (UIOP/RUN-PROGRAM:SUBPROCESS-ERROR nil)))
 
-(defvar *default-terminals*
-  (vector (uiop:getenv "TERMINAL")
-          "alacritty"
-          "ghostty"
-          "kitty"
-          "xfce4-terminal"
-          "konsole"
-          "gnome-terminal"
-          "wezterm"
-          "foot")
-  "A vector of default terminal programs to use.")
-
-(defun open-terminal ()
+(defun open-program (candidates)
   ;; TODO add log messages here for the case where no provided names work.
-  (loop :for name :across *default-terminals*
+  (loop :for name :in candidates
 		:do (alex:when-let ((program (find-program name)))
               (uiop:launch-program program)
               (return t)))
   (values nil))
+
+(defvar *default-terminals*
+  `(,(uiop:getenv "TERMINAL")
+    "alacritty" "ghostty" "kitty" "xfce4-terminal"
+    "konsole" "gnome-terminal" "wezterm" "foot")
+  "A list of default terminal programs to use.")
+
+(defun open-terminal ()
+  (open-program *default-terminals*))
