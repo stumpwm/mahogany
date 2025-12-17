d4bfaf81056392ee47802c74c311e36abe9e4e3e
jcguu95 <jcguu95@gmail.com>
2025-12-17 09:39:38 -0700

    better support default terminals
    1. User can now define the vector of terminals they want to use by
       customizing *DEFAULT-TERMINALS*.
    2. Get rid of IF-LET* as it's not needed elsewhere in the codebase.

--- lisp/system.lisp
+++ lisp/system.lisp
@@ -17,13 +17,22 @@ Mahogany is running under")
 		     (uiop:run-program (list "which" name) :output stream)))
     (UIOP/RUN-PROGRAM:SUBPROCESS-ERROR nil)))
 
+(defvar *default-terminals*
+  (vector (uiop:getenv "TERMINAL")
+          "alacritty"
+          "ghostty"
+          "kitty"
+          "xfce4-terminal"
+          "konsole"
+          "gnome-terminal"
+          "wezterm"
+          "foot")
+  "A vector of default terminal programs to use.")
+
 (defun open-terminal ()
-  (if-let* ((term (uiop:getenv "TERMINAL"))
-			(prog-path (find-program term)))
-      (uiop:launch-program prog-path)
-	(let ((programs #("konsole" "gnome-terminal" "wezterm" "foot")))
-      (loop for i across programs
-			do (alex:when-let ((program (find-program i)))
-				 (uiop:launch-program program)
-				 (return t)))
-      (values nil))))
+  ;; TODO add log messages here for the case where no provided names work.
+  (loop :for name :across *default-terminals*
+		:do (alex:when-let ((program (find-program name)))
+              (uiop:launch-program program)
+              (return t)))
+  (values nil))
