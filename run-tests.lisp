0f080c8599410b30db8b68d5c5cc6ba379823b1c
Stuart Dilts <stuart.dilts@gmail.com>
2025-12-18 18:29:52 -0700

    CI/CD: Exit with error code when tests fail

--- run-tests.lisp
+++ run-tests.lisp
@@ -2,4 +2,10 @@
 
 (load "init-build-env.lisp")
 
-(asdf:test-system "mahogany")
+(asdf:load-system "mahogany-test")
+
+;; (asdf:test-system "mahogany")
+(let ((result (fiasco:all-tests)))
+  (if result
+      (uiop:quit 0)
+      (uiop:quit 1)))
