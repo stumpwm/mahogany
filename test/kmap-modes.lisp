9d71efac8652fce456262dc0eb1823eaf23aba50
Stuart Dilts <stuart.dilts@gmail.com>
2026-01-02 21:53:38 -0700

    Fix kmap-mode name validation error message
    Checking that the symbol isn't just `-mode` after checking that it
    ends in `-mode` gives us the right error message.

--- test/kmap-modes.lisp
+++ test/kmap-modes.lisp
@@ -5,4 +5,9 @@
 
 (fiasco:deftest define-kmap-mode-signals-when-name-wrong ()
   (signals simple-error (macroexpand `(mahogany::define-kmap-mode foo)))
+  (signals simple-error (macroexpand '(mahogany::define-kmap-mode df)))
   (signals simple-error (macroexpand '(mahogany::define-kmap-mode -mode))))
+
+(fiasco:deftest define-kmap-mode-works-when-name-correct ()
+  (is (macroexpand '(mahogany::define-kmap-mode test-mode
+                     :prefix-binding *kmap*))))
