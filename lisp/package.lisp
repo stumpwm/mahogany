3ba7493b7dddc084f9ecc13a38d3184478d9ac77
Stuart Dilts <stuart.dilts@gmail.com>
2026-01-03 12:42:09 -0700

    Add mahogany-user package and public/private inteface doc
    Start developing the public vs unstable interfaces by adding
    documentation and creating the `mahogany-user` package to make writing
    configurations a bit more streamlined.

--- lisp/package.lisp
+++ lisp/package.lisp
@@ -6,3 +6,8 @@
         #:mahogany/util
         #:mahogany/keyboard)
   (:local-nicknames (#:tree #:mahogany/tree)))
+
+(defpackage #:mahogany-user
+  (:use :cl
+        #:mahogany)
+  (:documentation "User package for the mahogany system."))
