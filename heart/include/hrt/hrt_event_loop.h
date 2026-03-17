f3df17112ed90f965ef9a5ba8d39da60458bfaa5
Stuart Dilts <stuart.dilts@gmail.com>
2026-03-17 15:00:41 -0600

    hrt: Add ability to run function when a semaphore is non-zero
    This adds a dependency to the eventfd system call, which is available
    on all of the platforms that wlroots suppports.

--- heart/include/hrt/hrt_event_loop.h
+++ heart/include/hrt/hrt_event_loop.h
@@ -22,4 +22,32 @@ struct wl_event_source *hrt_event_loop_add_fd(struct hrt_server *server, int fd,
 
 void hrt_event_loop_remove(struct wl_event_source *source);
 
+struct hrt_fd_semaphore {
+    int fd;
+    struct wl_event_source *event_source;
+};
+
+/**
+ * Create a semaphore fd (via eventfd(2)), and call the provided function
+ * whenever the semaphore is non-zero.
+ **/
+struct hrt_fd_semaphore *
+hrt_event_loop_semaphore_add(struct hrt_server *server, int initval,
+                             hrt_event_loop_fd_func_t callback);
+
+/**
+ * Increment the given semaphore by the given value.
+ **/
+bool hrt_event_loop_semaphore_increment(struct hrt_fd_semaphore *fd, uint64_t increment);
+
+/**
+ * Decrement the semaphore by 1
+ **/
+bool hrt_event_loop_semaphore_decrement(struct hrt_fd_semaphore *fd);
+
+/**
+ * Close the semaphore fd and remove it from the event loop.
+ **/
+void hrt_event_loop_semaphore_close(struct hrt_fd_semaphore *fd);
+
 #endif
