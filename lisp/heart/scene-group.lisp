f3e06d2021de9701b1e4e167c962a7ef2ca0a4bf
Stuart Dilts <stuart.dilts@gmail.com>
2026-03-17 15:58:45 -0600

    lisp: Fix error checking in scene-create-fullscreen-node

--- lisp/heart/scene-group.lisp
+++ lisp/heart/scene-group.lisp
@@ -10,11 +10,12 @@
 (defun scene-create-fullscreen-node (hrt-group view hrt-output)
   (declare (type view view)
            (type cffi:foreign-pointer hrt-group hrt-output))
-  (alexandria:if-let ((node (hrt-scene-create-fullscreen-node hrt-group
-                                                              (view-hrt-view view)
-                                                              hrt-output)))
-    node
-    (error "Could not allocate fullscreen node.")))
+  (let ((node (hrt-scene-create-fullscreen-node hrt-group
+                                                (view-hrt-view view)
+                                                hrt-output)))
+    (if (not (cffi:null-pointer-p node))
+        node
+        (error "Could not allocate fullscreen node."))))
 
 
 (declaim (inline scene-init-view))
