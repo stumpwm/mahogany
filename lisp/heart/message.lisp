stumpwm/mahogany

commit 4801d2c3b77ac8203f5aa13125e28137c49e5982

hrt: Move (and rename) mahogany-output to hrt wrappers
Since the output object is a simple wrapper and doesn't have any
front-end specific functions, move the implementation to the hrt
package to make referencing it in other places in the hrt package
possible.
+ Should improve typechecking
+ Makes most usages less verbose

lisp/heart/message.lisp
@@ -16,7 +16,8 @@
 		      gravity
 		      theme
 		      ms-delay)
-  (declare (type cffi:foreign-pointer server output)
+  (declare (type cffi:foreign-pointer server)
+           (type hrt:output output)
 	   (type toast-message-gravity gravity)
 	   (type integer ms-delay)
 	   (type mh/theme:theme theme)

lisp/message.lisp
@@ -20,8 +20,7 @@
                    (hrt-server mahogany-state-server))
       state
     (let* ((cur-output (group-current-output cur-group)))
-      (alexandria:when-let ((hrt-output (mahogany-output-hrt-output cur-output)))
-        (hrt:toast-message hrt-server hrt-output text
-                           gravity
-                           theme
-                           ms-delay)))))
+      (hrt:toast-message hrt-server cur-output text
+                         gravity
+                         theme
+                         ms-delay))))

lisp/group.lisp
@@ -91,7 +91,7 @@
     (setf current-frame nil)))
 
 (defun group-add-output (group output seat)
-  (declare (type mahogany-output output)
+  (declare (type hrt:output output)
            (type mahogany-group group))
   (with-accessors ((output-map mahogany-group-output-map)
                    (tiled-container mahogany-group-tiled-container)
@@ -99,13 +99,13 @@
                    (hidden-views mahogany-group-hidden-views))
       group
     (multiple-value-bind (x y)
-        (hrt:output-position (mahogany-output-hrt-output output))
+        (hrt:output-position output)
       (multiple-value-bind (width height)
-          (hrt:output-resolution (mahogany-output-hrt-output output))
+          (hrt:output-resolution output)
         (let ((new-tree (tree:tree-output-add tiled-container output
                                               :x x :y y
                                               :width width :height height)))
-          (setf (gethash (mahogany-output-full-name output) output-map) new-tree)
+          (setf (gethash (hrt:output-full-name output) output-map) new-tree)
           (when (not current-frame)
             (let ((first-leaf (tree:find-first-leaf new-tree)))
               (group-focus-frame group first-leaf seat)
@@ -117,12 +117,11 @@
   "Re-examine where the outputs are and adjust the trees that are associated with them
 to match."
   (with-accessors ((output-map mahogany-group-output-map)) group
-    (loop for mh-output across outputs
-          do (with-accessors ((full-name mahogany-output-full-name)
-                              (hrt-output mahogany-output-hrt-output))
-                 mh-output
+    (loop for output across outputs
+          do (with-accessors ((full-name hrt:output-full-name))
+                 output
                (alexandria:when-let ((tree (gethash full-name output-map)))
-                 (tree:reconfigure-node tree hrt-output))))))
+                 (tree:reconfigure-node tree output))))))
 
 (defun %first-hash-table-value (table)
   (declare (type hash-table table)
@@ -139,12 +138,12 @@ to match."
     (tree:output-node-output output-node)))
 
 (defun group-remove-output (group output seat)
-  (declare (type mahogany-output output)
+  (declare (type hrt:output output)
            (type mahogany-group group))
   (with-accessors ((output-map mahogany-group-output-map)
                    (hidden-views mahogany-group-hidden-views))
       group
-    (let* ((output-name (mahogany-output-full-name output))
+    (let* ((output-name (hrt:output-full-name output))
            (tree (gethash output-name output-map)))
       (remhash output-name output-map)
       (when (equalp output (group-current-output group))

lisp/heart/output.lisp
@@ -1,18 +1,60 @@
 (in-package #:hrt)
 
+(defstruct (output (:constructor %make-output
+					  (hrt-output full-name)))
+  (hrt-output cffi:null-pointer :type cffi:foreign-pointer :read-only t)
+  (full-name "" :type string :read-only t))
+
+(defun %get-output-full-name (hrt-output)
+  (let ((make (hrt-output-make hrt-output))
+	(name (hrt-output-name hrt-output))
+	(serial (hrt-output-serial hrt-output))
+	(model (hrt-output-model hrt-output)))
+    (concatenate 'string
+		 (or name "")
+		 (or make "")
+		 (or model "")
+		 (or serial ""))))
+
+(defun make-output (hrt-output)
+  (declare (type cffi:foreign-pointer hrt-output))
+  (let ((name (%get-output-full-name hrt-output)))
+    (%make-output hrt-output name)))
+
+(defun destroy-output (mh-output)
+  (declare (ignore mh-output)))
+
 (defun output-resolution (output)
-  (declare (type cffi:foreign-pointer output))
+  (declare (type output output))
   (with-return-by-value ((width :int) (height :int))
-    (hrt-output-resolution output width height)))
+    (hrt-output-resolution (output-hrt-output output) width height)))
 
 (defun output-position (output)
-  (declare (type cffi:foreign-pointer output))
+  (declare (type output output))
   (with-return-by-value ((x :int) (y :int))
-    (hrt-output-position output x y)))
+    (hrt-output-position (output-hrt-output output) x y)))
 
 (defun output-usable-area (output)
-  (declare (type cffi:foreign-pointer output))
-  (let ((box (cffi:foreign-slot-pointer output '(:struct hrt-output)
+  (declare (type output output))
+  (let* ((hrt-output (output-hrt-output output))
+         (box (cffi:foreign-slot-pointer hrt-output '(:struct hrt-output)
                                       'usable-area)))
     (cffi:with-foreign-slots ((x y width height) box (:struct wlr-box))
       (values x y width height))))
+
+(declaim (inline output-name output-make output-model output-serial))
+(defun output-name (output)
+  (declare (type output output))
+  (hrt-output-name (output-hrt-output output)))
+
+(defun output-make (output)
+  (declare (type output output))
+  (hrt-output-make (output-hrt-output output)))
+
+(defun output-model (output)
+  (declare (type output output))
+  (hrt-output-serial (output-hrt-output output)))
+
+(defun output-serial (output)
+  (declare (type output output))
+  (hrt-output-serial (output-hrt-output output)))

lisp/heart/package.lisp
@@ -36,11 +36,11 @@
            #:hrt-seat-cursor-ly
            #:hrt-seat-set-keymap
            ;; output symbols:
+           #:output
            #:hrt-output
-           #:hrt-output-name
-           #:hrt-output-make
-           #:hrt-output-model
-           #:hrt-output-serial
+           #:output-hrt-output
+           #:make-output
+           #:destroy-output
            #:hrt-keypress-info
            ;; output callbacks
            #:output-added
@@ -50,6 +50,11 @@
            #:output-resolution
            #:output-position
            #:output-usable-area
+           #:output-full-name
+           #:output-name
+	   #:output-make
+	   #:output-model
+	   #:output-serial
            ;; view-methods
            #:view
            #:view-init

lisp/heart/view.lisp
@@ -48,6 +48,7 @@
 (declaim (inline view-configure))
 (defun view-configure (view)
   (declare (type view view))
+  (log-string :trace "Sending configure to view ~A" view)
   (hrt-view-send-configure (view-hrt-view view)))
 
 (defmethod mh/interface:set-dimensions ((view view) width height)

lisp/objects.lisp
@@ -1,10 +1,5 @@
 (in-package #:mahogany)
 
-(defstruct (mahogany-output (:constructor %make-mahogany-output
-                                          (hrt-output full-name)))
-  (hrt-output cffi:null-pointer :type cffi:foreign-pointer :read-only t)
-  (full-name "" :type string :read-only t))
-
 (defstruct (mahogany-group (:constructor %make-mahogany-group
                                (name number hrt-group tiled-container)))
   (name "" :type string)
@@ -35,9 +30,9 @@
                :initform (kbd "C-t")
                :reader mahogany-state-prefix-key
                :documentation "The prefix key used for prefix-bound kmaps.")
-   (outputs :type (vector mahogany-output *)
+   (outputs :type (vector hrt:output *)
             :initform (make-array 0
-                                  :element-type 'mahogany-output
+                                  :element-type 'hrt:output
                                   :adjustable t
                                   :fill-pointer t)
             :accessor mahogany-state-outputs)

lisp/output.lisp
@@ -1,19 +1,0 @@
-(in-package #:mahogany)
-
-(defun %get-output-full-name (hrt-output)
-  (let ((make (hrt:hrt-output-make hrt-output))
-        (name (hrt:hrt-output-name hrt-output))
-        (serial (hrt:hrt-output-serial hrt-output))
-        (model (hrt:hrt-output-model hrt-output)))
-    (concatenate 'string
-                 (or name "")
-                 (or make "")
-                 (or model "")
-                 (or serial ""))))
-
-(defun make-mahogany-output (hrt-output)
-  (let ((name (%get-output-full-name hrt-output)))
-    (%make-mahogany-output hrt-output name)))
-
-(defun destroy-mahogany-output (mh-output)
-  (declare (ignore mh-output)))

commit 48c478401a41ca1ec0a747c56b92c29a79fb2971

lisp: fix arg output arg to hrt-toast-message
The output value needs to be unwrapped.

Broken in 4801d2c.

lisp/heart/message.lisp
@@ -42,4 +42,10 @@
 						    '(:struct hrt-message-theme)
 						    'border-color)
 			 (mh/theme:theme-border-color theme))
-      (hrt-toast-message server output m gravity message-theme ms-delay))))
+      (hrt-toast-message
+       server
+       (output-hrt-output output)
+       m
+       gravity
+       message-theme
+       ms-delay))))