5e1dd8dfa25d5caae3813fbedd1a8f670ca8e6da
Stuart Dilts <stuart.dilts@gmail.com>
2026-05-17 21:52:34 -0600

    build: Use init-build-env file in run-main.lisp

--- run-main.lisp
+++ run-main.lisp
@@ -1,9 +1,6 @@
-(asdf:initialize-source-registry
- `(:source-registry
-   (:directory ,(uiop/os:getcwd))
-   (:tree ,(merge-pathnames (uiop/os:getcwd) #P"dependencies"))
-   ;; Use whatever the user has configured in their environment to find the rest.
-   :inherit-configuration))
+(require 'asdf)
+
+(load "init-build-env.lisp")
 
 (asdf:load-system "mahogany")
 
