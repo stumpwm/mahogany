6e525e4f9016f894bc517f6c035551678caf7cad
Stuart Dilts <stuart.dilts@gmail.com>
2026-06-25 11:40:57 -0600

    Remove dependency on cl-wayland
    Instead, just have our own declarations for the tiny subset of items
    we need. This reduces the image size and makes it more clear that we
    aren't really doing anything related to wayland in the lisp code. If
    we do go back to calling these functions, there are a few different
    libraries that may have gained better support by then.

---
+++ lisp/heart/wl-bindings.lisp
@@ -0,0 +1,25 @@
+(in-package #:wl)
+
+(cffi:defcstruct wl-list
+  (prev :pointer)
+  (next :pointer))
+
+(cffi:defcstruct wl-listener
+  "wl_listener struct"
+  (link (:struct wl-list))
+  (notify :pointer))
+
+(cffi:defcenum wl-output-transform
+  +output-transform-normal+
+  +output-transform-90+
+  +output-transform-180+
+  +output-transform-270+
+  +output-transform-flipped+
+  +output-transform-flipped-90+
+  +output-transform-flipped-180+
+  +output-transform-flipped-270+)
+
+(cffi:defcenum wl-keyboard-key-state
+  +wl-keyboard-key-state-released+
+  +wl-keyboard-key-state-pressed+
+  +wl-keyboard-key-state-repeated+)
