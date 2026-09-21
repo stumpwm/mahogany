stumpwm/mahogany

commit 171dbfa14a4237f8d2467d40ee277a47da02dfb0

Associate hrt:views with the frames they are in
This allows us to skip searching for them when a view related event
occurs.

lisp/tree/view.lisp
@@ -46,6 +46,14 @@
     (when view
       (hrt:hrt-border-box-set-enabled border-box nil))))
 
+(defmethod (setf frame-surface) :around (new-view (frame view-frame))
+  (let ((old-view (frame-surface frame)))
+    (call-next-method)
+    (when old-view
+      (setf (hrt::view-container old-view) nil))
+    (when new-view
+      (setf (hrt::view-container new-view) frame))))
+
 (defun cleanup-frame (frame)
   (log-string :trace "Cleaning up frame ~S" frame)
   (hrt:hrt-border-box-destroy (slot-value frame 'border-box))
@@ -96,8 +104,9 @@ and position as the frame"
   (print-unreadable-object (object stream :type t)
     (with-slots (width height x y view)
         object
-      (format stream ":w ~A :h ~A :x ~A :y ~A view: ~S"
-              (round width) (round height) (round x) (round y) view))))
+      (let ((*print-circle* t))
+        (format stream ":w ~A :h ~A :x ~A :y ~A view: ~S"
+                (round width) (round height) (round x) (round y) view)))))
 
 (defmethod (setf frame-x) :before (new-x (frame view-frame))
   (let ((round-x (round new-x))

lisp/heart/view.lisp
@@ -1,7 +1,8 @@
 (in-package #:hrt)
 
 (defstruct (view (:constructor %make-view (hrt-view)))
-  (hrt-view (cffi:null-pointer) :type cffi:foreign-pointer :read-only t))
+  (hrt-view (cffi:null-pointer) :type cffi:foreign-pointer :read-only t)
+  (container nil))
 
 #-hrt-debug
 (declaim (inline init-view))

lisp/group.lisp
@@ -234,7 +234,7 @@ to match."
                    (hidden mahogany-group-hidden-views))
       group
     (log-string :trace "unmapping view ~S" view)
-    (alexandria:if-let ((f (tree:find-view-frame (mahogany-group-tiled-container group) view)))
+    (alexandria:if-let ((f (hrt::view-container view)))
       (hrt:with-view-transaction ()
         (etypecase f
           (tree:output-node
@@ -270,7 +270,7 @@ to match."
       ;; if the view is hidden, it won't be in a frame:
       (or
        (ring-list:remove-item hidden view)
-       (alexandria:when-let ((f (tree:find-view-frame tiled-container view)))
+       (alexandria:when-let ((f (hrt::view-container view)))
          (setf (tree:frame-surface f) nil))))
     (setf view-list (remove view view-list :test #'equalp))))
 
@@ -338,9 +338,7 @@ currently focused frame"
            (type hrt:view view)
            (type mahogany-group group))
   (hrt:view-set-fullscreen view t)
-  (alexandria:if-let ((frame (tree:find-view-frame
-			      (mahogany-group-tiled-container group)
-			      view)))
+  (alexandria:if-let ((frame (hrt::view-container view)))
     ;; The frame is visible, immediately fullscreen it:
     (let ((output-node (if output
 			   (gethash (hrt:output-full-name output)
@@ -392,9 +390,7 @@ After this function is ran, the current frame needs to be set and focused."
   (declare (type mahogany-group group)
            (type hrt:view view))
   (hrt:view-set-fullscreen view nil)
-  (alexandria:if-let ((frame (tree:find-view-frame
-                              (mahogany-group-tiled-container group)
-                              view)))
+  (alexandria:if-let ((frame (hrt::view-container view)))
     ;; The frame is visible, we need to do some cleanup:
     (cond
       ((typep frame 'tree:output-node)
@@ -478,10 +474,8 @@ After this function is ran, the current frame needs to be set and focused."
   (declare (type mahogany-group group)
            (type hrt:view view))
   ;; attempt to stop abuse by only listening when the
-  ;; frame requesting this info is focused:
-  (alexandria:if-let ((frame (tree:find-view-frame
-                              (mahogany-group-current-frame group)
-                              view)))
+  ;; frame requesting this info is visible:
+  (alexandria:if-let ((frame (hrt::view-container view)))
     (progn
       (log-string :trace "maximizing view ~S" view)
       (%maximize-frame group frame))

lisp/tree/output-node.lisp
@@ -35,12 +35,16 @@ view, if there was one."
             (other-view (%fullscreen-data-view fullscreen-data)))
         (hrt:scene-fullscreen-swap fullscreen-node view)
         (hrt:scene-fullscreen-configure fullscreen-node output)
-        (setf (%fullscreen-data-view fullscreen-data) view)
+        (setf (%fullscreen-data-view fullscreen-data) view
+              (hrt::view-container other-view) nil
+              (hrt::view-container view) output-node)
         other-view)
+      ;; There is no fullscreen item:
       (let* ((hrt-layer (layer-container-layer (frame-parent output-node)))
              (fullscreen-node (hrt:scene-create-fullscreen-node hrt-layer view output)))
         (log-string :trace "Created new fullscreen node for output")
-        (setf (output-node-fullscreen output-node) (%make-fullscreen-data view fullscreen-node))
+        (setf (output-node-fullscreen output-node) (%make-fullscreen-data view fullscreen-node)
+              (hrt::view-container view) output-node)
         nil))))
 
 (defun %find-first-child (frame)
@@ -68,7 +72,8 @@ view, if there was one."
     (let ((node (%fullscreen-data-node data))
           (view (%fullscreen-data-view data)))
       (hrt:hrt-scene-fullscreen-node-destroy node)
-      (setf (output-node-fullscreen output-node) nil)
+      (setf (output-node-fullscreen output-node) nil
+            (hrt::view-container view) nil)
       view)))
 
 (defmethod mark-frame-focused :after ((frame output-node) seat)
@@ -135,6 +140,7 @@ view, if there was one."
   (print-unreadable-object (object stream :type t)
     (with-slots (children output fullscreen)
         object
-      (format stream
-              "~:_:output ~S ~:_:fullscreen ~S ~:_:children ~S"
-              output fullscreen children))))
+      (let ((*print-circle* t))
+        (format stream
+                "~:_:output ~S ~:_:fullscreen ~S ~:_:children ~S"
+                output fullscreen children)))))

commit 4d88ea4bd3721e705f7911465e246c1563363887

Clear the association between a view and its frame on destroy
Broken in 171dbfa.

lisp/tree/view.lisp
@@ -57,7 +57,9 @@
 (defun cleanup-frame (frame)
   (log-string :trace "Cleaning up frame ~S" frame)
   (hrt:hrt-border-box-destroy (slot-value frame 'border-box))
-  (setf (slot-value frame 'border-box) nil))
+  (setf (slot-value frame 'border-box) nil)
+  (alexandria:when-let ((view (frame-surface frame)))
+    (setf (hrt::view-container view) nil)))
 
 (defmethod replace-frame ((root view-frame) frame &optional (cleanup-func #'identity))
   (unless (eql root frame)