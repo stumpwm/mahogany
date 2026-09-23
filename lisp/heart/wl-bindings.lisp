87887a92a48b83a9b0dd20c4670e88624df1ac6d
Stuart Dilts <stuart.dilts@gmail.com>
2026-09-22 23:00:29 -0600

    wl: Add wl-list-for-each

--- lisp/heart/wl-bindings.lisp
+++ lisp/heart/wl-bindings.lisp
@@ -1,8 +1,34 @@
 (in-package #:wl)
 
+;; This should really be a function, but
+;; I couldn't get SBCL to use `foreign-slot-offset`'s compiler
+;; macro (which looks up the value at compile time)
+;; by making this a function and marking it as inline.
+;; It appears that compiler macros aren't expanded after inlining,
+;; which makes sense depending on what intermediate representation
+;; actually gets inlined.
+(defmacro container-of (ptr type member)
+  `(cffi:make-pointer (- (cffi:pointer-address ,ptr)
+                         (cffi:foreign-slot-offset ,type ,member))))
+
 (cffi:defcstruct wl-list
   (prev :pointer)
   (next :pointer))
+
+(defmacro list-for-each ((elem (head member type)) &body body)
+  (let ((head-var (gensym "head"))
+        (cur-list (gensym "cur-list")))
+    ;; Follow the `next` pointers until we get back to where we started.
+    ;; The current element is grabbed using the container-of macro.
+    `(let* ((,head-var ,head))
+       (do* ((,cur-list (cffi:foreign-slot-value ,head-var '(:struct wl:wl-list)
+                                                 'next)
+                        (cffi:foreign-slot-value ,cur-list '(:struct wl:wl-list)
+                                                 'next))
+             (,elem (container-of ,cur-list ,type ,member)
+                    (container-of ,cur-list ,type ,member)))
+            ((cffi:pointer-eq ,cur-list ,head-var))
+         ,@body))))
 
 (cffi:defcstruct wl-listener
   "wl_listener struct"
